3bf9f4bc94f10f636790019b1bcf54b0696cf98b
0xf000000 <Leon@margale.de>
2023-12-14 14:09:23 +0100

    exercice finished

--- exercise.py
+++ exercise.py
@@ -1,13 +1,13 @@
 import bz2
 
 import numpy as np
-
+import tensorflow as tf
 from tensorflow.python.keras import models, layers
 from tensorflow.keras.preprocessing.text import Tokenizer, text_to_word_sequence
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras import regularizers
 
-
+print(tf.__version__)
 def get_labels_and_texts(file, n=10000):
     labels = []
     texts = []
@@ -22,11 +22,23 @@ def get_labels_and_texts(file, n=10000):
     return np.array(labels), texts
 
 
-train_labels, train_texts = get_labels_and_texts('data/train.ft.txt.bz2')
+train_labels, train_texts = get_labels_and_texts('data/train.bz2')
+
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(train_texts)
+vocabSize = len(tokenizer.word_index) +1 
+
+train_texts = tokenizer.texts_to_sequences(train_texts)
+MAX_LENGTH = max(len(train_ex) for train_ex in train_texts)
+
+train_texts =  pad_sequences(train_texts,maxlen=MAX_LENGTH, padding="post")
+
 
 
 ffnn = models.Sequential()
 ffnn.add(layers.Input(shape=(MAX_LENGTH)))
+ffnn.add(layers.Embedding(vocabSize, 200, input_length=MAX_LENGTH))
+ffnn.add(layers.Flatten())
 ffnn.add(layers.Dense(100, activation="sigmoid"))
 ffnn.add(layers.Dropout(0.5))
 ffnn.add(layers.Dense(50, activation="sigmoid"))
@@ -38,6 +50,6 @@ ffnn.summary()
 ffnn.compile(loss="binary_crossentropy", optimizer="sgd",
   metrics=["accuracy"])
 
-ffnn.fit(train_texts, train_labels, epochs=10, batch_size=10, verbose=1)
+ffnn.fit(train_texts, train_labels, epochs=10, batch_size=10)
 
-
+## okok if we put more into it, it should get really good
