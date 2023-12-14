6432a1b8ae64fa8fc41d742775963fbd1de33ff5
Mw1701 <mwerk1@smail.uni-koeln.de>
2023-12-14 14:08:49 +0100

    versions...

--- exercise.py
+++ exercise.py
@@ -2,10 +2,18 @@ import bz2
 
 import numpy as np
 
-from tensorflow.python.keras import models, layers
-from tensorflow.keras.preprocessing.text import Tokenizer, text_to_word_sequence
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras import regularizers
+#from tensorflow.python.keras import models, layers
+# from tensorflow.keras.preprocessing.text import Tokenizer, text_to_word_sequence
+# from tensorflow.keras.preprocessing.sequence import pad_sequences
+# from tensorflow.keras import regularizers
+import keras
+import tensorflow as tf
+from keras.preprocessing.text import Tokenizer, text_to_word_sequence   #version Problem
+from keras.preprocessing.sequence import pad_sequences    #version Problem
+from keras import layers
+from keras import regularizers
+
+#Versionsprobleme irgendwie damit behoben
 
 
 def get_labels_and_texts(file, n=10000):
@@ -22,12 +30,28 @@ def get_labels_and_texts(file, n=10000):
     return np.array(labels), texts
 
 
-train_labels, train_texts = get_labels_and_texts('data/train.ft.txt.bz2')
+train_labels, train_texts = get_labels_and_texts('exercise-08/data/train.ft.txt.bz2', 13000)
+#n verändern?...
 
 
-ffnn = models.Sequential()
+#preprocess
+#train_texts to integer array ...
+
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(train_texts)
+vocab_size = len(tokenizer.word_index) + 1
+train_texts = tokenizer.texts_to_sequences(train_texts)
+
+MAX_LENGTH = max(len(train_ex) for train_ex in train_texts)
+
+train_texts = pad_sequences(train_texts , maxlen=MAX_LENGTH , padding="post")
+
+
+ffnn = tf.keras.models.Sequential()
 ffnn.add(layers.Input(shape=(MAX_LENGTH)))
-ffnn.add(layers.Dense(100, activation="sigmoid"))
+ffnn.add(layers.Embedding(vocab_size , 200, input_length=MAX_LENGTH))
+ffnn.add(layers.Flatten())
+ffnn.add(layers.Dense(100, activation="sigmoid", activity_regularizer=regularizers.L2(0.4)))
 ffnn.add(layers.Dropout(0.5))
 ffnn.add(layers.Dense(50, activation="sigmoid"))
 ffnn.add(layers.Dropout(0.5))
@@ -41,3 +65,9 @@ ffnn.compile(loss="binary_crossentropy", optimizer="sgd",
 ffnn.fit(train_texts, train_labels, epochs=10, batch_size=10, verbose=1)
 
 
+# 200 (n=10000) -> 0,5084 (l=0,6967)
+# 300 -> 0,5050 ; +longer (l=0,6972)
+
+#Data Set
+# 200 und n= 10000 -> 0,5084 (l=0,6967)
+# 200 und n= 13000 -> 0,5030 (l=0,6972)
