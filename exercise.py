33e2630ab5ca7bfa4f6cd1afa618bd57418ac1ed
mhrka <mhrkac@smail.uni-koeln.de>
2023-12-15 11:12:06 +0100

    embeddings

--- exercise.py
+++ exercise.py
@@ -22,11 +22,23 @@ def get_labels_and_texts(file, n=10000):
     return np.array(labels), texts
 
 
-train_labels, train_texts = get_labels_and_texts('data/train.ft.txt.bz2')
+train_labels, train_texts = get_labels_and_texts('../exercise-07/data/train.ft.txt.bz2')
+
+
+tokenizer = Tokenizer ()
+tokenizer.fit_on_texts(train_texts)
+vocab_size = len(tokenizer.word_index) + 1
+train_texts = tokenizer.texts_to_sequences(train_texts)
+
+MAX_LENGTH = max(len(train_ex) for train_ex in train_texts)
+
+train_texts = pad_sequences(train_texts, maxlen = MAX_LENGTH, padding ="post")
 
 
 ffnn = models.Sequential()
 ffnn.add(layers.Input(shape=(MAX_LENGTH)))
+ffnn.add(layers.Embedding(vocab_size, 200, input_length = MAX_LENGTH))
+ffnn.add(layers.Flatten())
 ffnn.add(layers.Dense(100, activation="sigmoid"))
 ffnn.add(layers.Dropout(0.5))
 ffnn.add(layers.Dense(50, activation="sigmoid"))
@@ -35,9 +47,8 @@ ffnn.add(layers.Dense(1, activation="sigmoid"))
 
 ffnn.summary()
 
-ffnn.compile(loss="binary_crossentropy", optimizer="sgd",
-  metrics=["accuracy"])
+ffnn.compile(loss="binary_crossentropy", optimizer="sgd", metrics=["accuracy"])
 
 ffnn.fit(train_texts, train_labels, epochs=10, batch_size=10, verbose=1)
 
-
+#loss 0.6947, accuracy 0.5038
